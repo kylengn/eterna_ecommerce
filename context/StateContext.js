b12bbed4aa7c9a4ef761af73ab2c10d7b5c94736
Kyle Nguyen <kyle.nguyen304@gmail.com>
2022-06-11 15:07:31 +0700

    fixed checkout decrement bug

--- context/StateContext.js
+++ context/StateContext.js
@@ -73,7 +73,7 @@ export const StateContext = ({ children }) => {
           ...newCartItems,
           { ...foundProduct, quantity: foundProduct.quantity - 1 },
         ]);
-        setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
       }
     }
